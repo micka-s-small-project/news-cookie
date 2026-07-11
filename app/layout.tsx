1fe6d5db2b8229553bd6ca5c51d3a5fbab7656c2
yellTa <32352161+yellTa@users.noreply.github.com>
2026-07-11 15:45:10 +0900

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   title: "News Cookie | AI-Powered News Insights 🍪",
   description: "Bake your complex queries into crystal-clear news insight reports and store them in your jar.",
   icons: {
-    icon: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍪</text></svg>`,
+    icon: "/favicon.svg",
   },
 
   keywords: [
